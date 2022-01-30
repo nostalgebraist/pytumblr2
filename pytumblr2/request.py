a941c84c1653b56d9451bd65aa5fbad77733ac08
nostalgebraist <nostalgebraist@gmail.com>
2022-01-30 14:33:39 -0800

    version to 0.2.0

--- pytumblr2/request.py
+++ pytumblr2/request.py
@@ -12,7 +12,7 @@ class TumblrRequest(object):
     A simple request object that lets us query the Tumblr API
     """
 
-    __version = "0.1.0"
+    __version = "0.2.0"
 
     def __init__(
         self,
